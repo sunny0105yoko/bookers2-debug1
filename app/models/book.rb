90a327518212200d75e63548b47e5aaa919fe4a1
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-06-05 02:28:03 +0000

    [started]応用課題７

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,12 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user#favoritesモデルを介してuserモデルのデータを持ってくる
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
-    favorites.exists?(user_id: user.id)
+    favorites.where(user_id: user.id).exists?
   end
   # 検索方法分岐
   def self.looks(search, word)
